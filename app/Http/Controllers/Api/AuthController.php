d5209d32cc57e59209823d337bc0c5fee214ee90
CodeCraftBySouvik <souvikmandal.sm19@gmail.com>
2025-03-25 20:22:17 +0530

    Modified in authcontroller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -555,7 +555,7 @@ class AuthController extends Controller
             return $user; // Return the response if the user is not authenticated
         }
         $filter = $request->keyword;
-
+        
         // Fetch filtered users
         $users = User::where('user_type', 1)
             ->where('status', 1)
@@ -585,18 +585,18 @@ class AuthController extends Controller
                 $users->prepend($order->customer);
             }
             
-            $data = $users->map(function ($user) {
-                return [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'phone' => $user->phone,
-                ];
-            });
+            // $data = $users->map(function ($user) {
+            //     return [
+            //         'id' => $user->id,
+            //         'name' => $user->name,
+            //         'email' => $user->email,
+            //         'phone' => $user->phone,
+            //     ];
+            // });
         return response()->json([
             'status' => true,
             'message' => 'Data fetched successfully!',
-            'data' => $data,
+            'data' => $users,
         ],200);
     }
     public function customer_store(Request $request){
